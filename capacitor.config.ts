c6c40e6a90759389394b2a26482686c798f7e45c
FranciscoBelda <francisco.belda@cieep.com>
2022-11-25 16:00:27 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'componentesDAMC2022',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
